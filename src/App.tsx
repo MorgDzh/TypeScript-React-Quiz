d3c4a7bbd7c7bce254b86b85743c975e719090d0
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-05-28 20:25:58 +0600

    added answer check

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { fetchQuizQuestions } from "./API";
 // Components
 import QuestionCard from "./components/QuestionCard";
@@ -11,13 +11,13 @@ type AnswerObject = {
   answer: string;
   correct: boolean;
   correctAnswer: string;
-}
+};
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  // Говорим тайпскрипту что это будет массив 
+  // Говорим тайпскрипту что это будет массив
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
@@ -41,7 +41,6 @@ const App = () => {
     setUserAnswers([]);
     setNumber(0);
     setLoading(false);
-
   };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -51,21 +50,35 @@ const App = () => {
   return (
     <div className="App">
       <h1>TYPESCRIPT QUIZ</h1>
-      <button className="start" onClick={startTrivia}>
-        Start
-      </button>
-      <p className="score">Score:</p>
-      <p>Loading Questions...</p>
+      {/* Проверка, если все ответы равны 10 то вернет null */}
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading && <p>Loading Questions...</p>}
       {/* Без пропсов будет компонент будет ругаться */}
-      {/* <QuestionCard 
-        questionNum={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={userAnswers ? userAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className="next" onClick={nextQuestion}>Next Question</button>
+      {/* Если игра не окончена, будет показана карта с вопросом */}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNum={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {/* Если юзер не ответил на вопрос кнопка не появится, если ответил то появится(number + 1 = типо ответ юзера) */}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 };
