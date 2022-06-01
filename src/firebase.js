4a7ce2ea674f5629e61897c7ca9f05ad8567b349
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-01 13:37:43 +0600

    added env

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {config} from 'dotenv'
+config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAKHywHbgY53cR36wQ8dueiCcIMFA4eEzw",
-  authDomain: "quiztypescript.firebaseapp.com",
-  projectId: "quiztypescript",
-  storageBucket: "quiztypescript.appspot.com",
-  messagingSenderId: "162771881171",
-  appId: "1:162771881171:web:b5bb64895ac9b89e819826",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
 };
 
 // Initialize Firebase
