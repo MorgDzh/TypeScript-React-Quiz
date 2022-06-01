600b2fd0d21aaeb58a1f8b7008f472418621e914
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-01 12:58:55 +0600

    deployed for fun

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKHywHbgY53cR36wQ8dueiCcIMFA4eEzw",
+  authDomain: "quiztypescript.firebaseapp.com",
+  projectId: "quiztypescript",
+  storageBucket: "quiztypescript.appspot.com",
+  messagingSenderId: "162771881171",
+  appId: "1:162771881171:web:b5bb64895ac9b89e819826",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
